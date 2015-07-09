316373cd8cb3e9ba24e9c5089b8b3dd7db17a79d
luis maltos <maltos@localhost.localdomain>
2015-07-09 14:49:01 -0500

    misc functions for SQM

---
+++ include/SQM.h
@@ -0,0 +1,25 @@
+
+#ifndef _SQM_H
+#define _SQM_H
+
+#include <cmath>
+using namespace std;
+
+#define EPSILON 0.001
+
+struct response_unit {
+  int location;
+  double v;
+  double beta;
+};
+
+struct network {
+  int n;
+};
+
+typedef struct response_unit response_unit;
+typedef struct network network;
+
+#endif
+
+/* eof */
