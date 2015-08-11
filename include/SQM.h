2fe99ea71194514fb01c76256c349e649405037d
ma1t05 <maltosla@gmail.com>
2015-08-11 13:12:26 -0500

    Remove heuristic source

--- include/SQM.h
+++ include/SQM.h
@@ -7,17 +7,10 @@ using namespace std;
 
 #define EPSILON 0.001
 
-struct response_unit {
-  int location;
-  double v;
-  double beta;
-};
-
 struct network {
   int n;
 };
 
-typedef struct response_unit response_unit;
 typedef struct network network;
 
 #endif
--- include/instance-creator.h
+++ include/instance-creator.h
@@ -4,20 +4,12 @@
 
 #include <cstdlib>
 #include "point.h"
+#include "SQM_heuristic.h"
 
 #define MIN_X 0.0
 #define MAX_X 1024.0
 #define MIN_Y 0.0
 #define MAX_Y 1024.0
-
-struct SQM_instance {
-  int M; /* Number of customers */
-  int N; /* Number of potencial sites to locate a server */
-  point* V; /* Set of demand points */
-  point* W; /* Set of potencial locations sites */
-};
-
-typedef struct SQM_instance SQM_instance;
 
 SQM_instance* IC_create_instance (int n,int m);
 SQM_instance* IC_read_instance (string,string);
