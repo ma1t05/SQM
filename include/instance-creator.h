e93a1177199d2ddb58904662efb39c3b6dca3740
ma1t05 <maltosla@gmail.com>
2015-08-18 12:19:33 -0500

    Change the ploting of instance, to include solution centers

--- include/instance-creator.h
+++ include/instance-creator.h
@@ -14,7 +14,7 @@
 SQM_instance* IC_create_instance (int n,int m);
 SQM_instance* IC_read_instance (string,string);
 void IC_write_instance (SQM_instance*,string,string);
-void IC_plot_instance (string,string,string);
+void IC_plot_instance(SQM_instance*I,int*,string);
 
 #endif
 
