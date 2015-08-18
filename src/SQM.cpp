bb8904a066ec389fa5f9e2929972cbbde94998c5
ma1t05 <maltosla@gmail.com>
2015-08-18 12:20:02 -0500

    Add manage of parametr for care instances

--- src/SQM.cpp
+++ src/SQM.cpp
@@ -8,32 +8,28 @@ std::ofstream LogFile;
 
 int main(int argc,char *argv[]) {
   string filename;
+  int M_clients,N_sites;
+  int p,l;
+  double mu,f,v;
   stringstream LogName;
   SQM_instance *I;
-  int p;
-  double mu;
-  double f;
-  double v;
-  int M_clients;
-  int N_sites;
+  int *Sol;
 
-  if (argc < 8) {
+  if (argc < 9) {
     filename = "Pba";
-    M_clients = 50;
-    N_sites = 30;
-    p = 5;
-    mu = 60.0*24.0/20.0;
-    f = 0.006;
-    v = 40.0;
+    M_clients = 50; N_sites = 30;
+    p = 5; l = 3;
+    mu = 60.0*24.0/20.0; f = 0.006; v = 40.0;
   }
   else {
     filename = argv[1];
     M_clients = atoi(argv[2]);
     N_sites = atoi(argv[3]);
     p = atoi(argv[4]);
-    mu = atof(argv[5]);
-    f = atof(argv[6]);
-    v = atof(argv[7]);
+    l = atoi(argv[5]);
+    mu = atof(argv[6]);
+    f = atof(argv[7]);
+    v = atof(argv[8]);
   }
   
   srand(time(NULL));
@@ -44,8 +40,8 @@ int main(int argc,char *argv[]) {
   /* I = IC_read_instance(demand_file,facility_file); */
   I = IC_create_instance(M_clients,N_sites);
   IC_write_instance(I,filename+"_demand.ins",filename+"_facility.ins");
-  /*IC_plot_instance(filename,filename+"_demand.ins",filename+"_facility.ins");*/
-  SQM_model(I,p,p,mu,f,v);
+  Sol = SQM_model(I,p,l,mu,f,v);
+  IC_plot_instance(I,Sol,filename);		   
   /*Goldberg(I,p,mu,f);*/
   LogFile.close();
   cout << LogName.str() << endl;
