329b8043cd5a34e8c52b4b243dda30f0bbeb8673
ma1t05 <maltosla@gmail.com>
2015-11-04 14:29:15 -0600

    Add new definitions to log more for

--- include/log.h
+++ include/log.h
@@ -8,6 +8,9 @@ extern LogLevel logLevel;
 #define logError(A) ((logLevel >= LOG_ERROR)?((A),0):(0))
 #define logInfo(A) ((logLevel >= LOG_INFO)?((A),0):(0))
 #define logDebug(A) ((logLevel >= LOG_DEBUG)?((A),0):(0))
+#define LogError (logLevel >= LOG_ERROR)
+#define LogInfo (logLevel >= LOG_INFO)
+#define LogDebug (logLevel >= LOG_DEBUG)
 
 void debugBreak();
 
