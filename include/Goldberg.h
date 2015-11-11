818b763b40e41e1980167fd8d062be32574356b7
ma1t05 <maltosla@gmail.com>
2015-11-11 07:06:32 -0600

    Adjust dependencies for Goldberg library

--- include/Goldberg.h
+++ include/Goldberg.h
@@ -3,6 +3,7 @@
 #define _GOLDBERG_H_
 
 #include "SQM.h"
+#include "SQM_model.h"
 
 void Goldberg(SQM_instance*, int, float, float);
 void gnuplot_goldberg(SQM_instance*,int,IloCplex*,IloBoolVarArray*,BoolVarArrayMatrix*);
