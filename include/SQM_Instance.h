ba6ee3610a43d61b37f4f3dcbc4b0aebe3dbd5f3
Luis Maltos <maltos@localhost.localdomain>
2015-11-14 09:16:57 -0600

    Change the instance definition, from structure to a class

--- include/SQM_Instance.h
+++ include/SQM_Instance.h
@@ -10,12 +10,43 @@
 #define MIN_Y 0.0
 #define MAX_Y 1024.0
 
-SQM_instance* IC_create_instance (int /* clients */,int /* sites */);
-SQM_instance* IC_read_instance (string,string);
-SQM_instance* IC_load_instance (string);
-void IC_write_instance (SQM_instance*,string,string);
+class SQM_instance {
+private:
+  int M; /* Number of demand points */
+  int N; /* Number of potencial sites to locate a server */
+  point *V; /* Set of demand points */
+  point *W; /* Set of potencial locations sites */
+  int **a; /* List of lists of preferred sites */
+  double **Dist; /* Matrix of distances beetwen demand and sites */
+  void set_distances();
+  void set_preferred_servers();
+public:
+  SQM_instance(int m,int n);
+  SQM_instance(string);
+  SQM_instance(string,string);
+  ~SQM_instance();
+  void write(string,string);
+  int demand_points();
+  int potential_sites();
+  point* site(int i);
+  point* demand(int j);
+  int site_order(int i /* site */,int j /* demand */);
+  double distance(int i /* site */,int j /* demand */);
+}
+
+
+/* Note response_unit was changed to server */
+class server {
+  int location;
+  int past_location;
+  double v;
+  double beta;
+};
+
+SQM_instance(int /* demand points */,int /* sites */);
+SQM_instance(string);
+SQM_instance(string,string);
 void IC_plot_instance(SQM_instance*,int*,string);
-void IC_delete_instance(SQM_instance*);
 
 double** SQM_dist_matrix(SQM_instance*);
 
