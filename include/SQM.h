604f55d2c4d6b7c4a9c4605ee9a13650fd4684a1
Luis Maltos <maltos@localhost.localdomain>
2015-11-14 09:16:25 -0600

    Remove instance definition

--- include/SQM.h
+++ include/SQM.h
@@ -3,36 +3,13 @@
 #define _SQM_H
 
 #include <fstream>
-#include <math.h>
 #include "point.h"
 
-using namespace std;
-
-#define            EPSILON 0.001
-#define           TIME_MAX 1200.0
-#define     MINS_PER_BLOCK 60
+#define EPSILON 0.001
+#define TIME_MAX 1200.0
+/* */
+#define MINS_PER_BLOCK 60
 #define BLOCKS_PER_HORIZON 24
-
-struct network {
-  int n;
-};
-typedef struct network network;
-
-struct SQM_instance {
-  int M; /* Number of customers */
-  int N; /* Number of potencial sites to locate a server */
-  point* V; /* Set of demand points */
-  point* W; /* Set of potencial locations sites */
-};
-typedef struct SQM_instance SQM_instance;
-
-struct response_unit {
-  int location;
-  int past_location;
-  double v;
-  double beta;
-};
-typedef struct response_unit response_unit;
 
 extern std::ofstream LogFile;
 extern std::ofstream results;
