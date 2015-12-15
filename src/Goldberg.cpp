c6fb2b81a50e96edfad855b474fbf76a65e4bfb1
ma1t05 <maltosla@gmail.com>
2015-12-15 13:32:04 -0600

    Restore files that use ilog library

---
+++ src/Goldberg.cpp
@@ -0,0 +1,291 @@
+/*
+ * Modelo que determina ubicaciones de ajustadores
+ * 
+ */
+
+#include "Goldberg.h"
+#include <sstream>
+
+void Goldberg
+(SQM_instance* I, // Set of points
+ int p, // facilities
+ float mu, // rate parameter
+ float f) //
+{
+  IloEnv env;
+  try {
+    clock_t clocks = clock();
+    int i,j,k,r;
+    IloInt n = I->N,m= I->M;
+    IloNum f_i;
+    IloNum rho;
+    IloNum M = 10000.0;
+    point *client,*potencial_site;
+          
+    cout << "Comienza definicion del Modelo" << endl;
+    IloModel modelo(env);
+    
+    cout << "++ Variables ++" << endl;
+    IloNumVar S(env,0,IloInfinity,IloNumVar::Float,"S");
+    IloBoolVarArray x(env);
+    BoolVarArrayMatrix y(env,m);
+    
+    char VarName[16];
+    for(i = 0;i < n;i++){
+      sprintf(VarName,"x%d",i+1);
+      x.add(IloBoolVar(env,VarName));
+    }
+
+    cout << "Nombre las variables para facil identificacion" << endl;
+    for (i = 0;i < m;i++) {
+      BoolVarMatrix y_i(env,n);
+      for (j = 0;j < n;j++) {
+	IloBoolVarArray y_i_j(env);
+	for (k = 0;k < p;k++) {
+	  sprintf(VarName,"y_%d_%d_%d",i+1,j+1,k+1);
+	  y_i_j.add(IloBoolVar(env,VarName));
+	}
+	y_i[j] = y_i_j;
+      }
+      y[i] = y_i;
+    }
+
+    cout << "++ Restricciones ++" << endl;
+    cout << "Solo una instalacion ocupa la posion k del cliente i" << endl;
+    for (i = 0;i < m;i++) {
+      for (k = 0;k < p;k++) {
+	IloExpr Cover(env);
+	for (j = 0;j < n;j++) Cover += y[i][j][k];	
+	modelo.add(Cover == 1);
+	Cover.end();
+      }
+    }
+
+    cout << "La instalacion j solo puede ocupar una posicion del cliente i" << endl;
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	modelo.add(IloSum(y[i][j]) <= 1);
+      }
+    }
+
+    cout << "Instalaciones a abrir" << endl;
+    modelo.add(IloSum(x) == p);
+
+    cout << "Relacionar variables de localizacion y asignacion" << endl;
+    for(i = 0;i < m;i++){
+      for (j = 0;j < n;j++) {
+	for (k = 0;k < p;k++) 
+	  modelo.add(y[i][j][k] <= x[j]);
+      }
+    }
+
+    cout << "Restricion de orden de asignaicion" << endl;
+    client = I->V;
+    potencial_site = I->W;
+    NumMatrix O(env,m);
+    for (i = 0;i < m;i++)
+      O[i] = IloNumArray(env,n);
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	O[i][j] = dist(&(client[i]),&(potencial_site[j]));
+      }
+    }
+      
+    for (i = 0;i < m;i++) {
+      for (k = 1;k < p;k++) {
+	for (j = 0;j < n;j++) {
+	  IloExpr balance(env);
+	  for (r = 0;r < n;r++) {
+	    if (O[i][r] <= O[i][j])
+	      balance += y[i][r][k-1];
+	  }
+	  modelo.add(y[i][j][k] <= balance);
+	  balance.end();
+	}
+      }
+    }
+
+    // Cargas de trabajo
+    IloNum coef;
+    // rho from Daskin
+    rho = 0.0;
+    for (i = 0;i < m;i++)
+      rho += f * client[i].demand;
+    rho /= (mu * p);
+    cout << "rho = " << rho << endl;
+    // rho from ReVelle & Hogan
+    // pendiente
+    
+    for (j = 0;j < n;j++) {
+      IloExpr workload(env);
+      for (k = 0;k < p;k++) {
+	coef = (1 - rho) * pow(rho,k);
+	for (i = 0;i < m;i++) {
+	  f_i = f * client[i].demand;
+	  workload += f_i * coef * O[i][j]* y[i][j][k];
+	}
+      }
+      modelo.add(S >= workload);
+    }
+    
+    cout << "++ Funcion Objetivo ++" << endl;
+    modelo.add(IloMinimize(env,S));
+    clocks = clock() - clocks;
+    results << "," << clocks / CLOCKS_PER_SEC;
+
+    cout << "Resuelve modelo" << endl;
+    IloCplex cplex(modelo);
+    stringstream ModelName;
+    ModelName << "Goldberg-model_" << m << "_" << n << "_" << p << ".lp";
+    cplex.exportModel(ModelName.str().c_str());
+    results << "," << ModelName.str();
+
+    LogFile << "Solve Goldberg model" << endl;
+    LogFile << endl << "** Cplex Start **" << endl;
+    cplex.setOut(LogFile);
+    cplex.setParam(IloCplex::TiLim,TIME_MAX);
+
+    double CplexTime = cplex.getCplexTime();
+    if (cplex.solve()) {
+
+      double gap = (cplex.getObjValue() - cplex.getBestObjValue()) / cplex.getBestObjValue();
+      results << "," << cplex.getCplexTime() - CplexTime
+	      << "," << cplex.getStatus()
+	      << "," << cplex.getObjValue()
+	      << "," << gap
+	      << endl;
+      
+      LogFile << "Solution status: " << cplex.getStatus() << endl;
+      LogFile << "Maximum profit = " << cplex.getObjValue() << endl;
+      for (j = 0;j < n;j++) {
+	if (cplex.getValue(x[j]) > 0.5) LogFile << j+1 << " ";
+	//cout << cplex.getValue(y[j]);
+      }
+      cout << endl;
+
+      /*for(i = 0;i < n;i++){
+	cout << "-";
+      }
+      cout << endl;
+      for(i = 0;i < n;i++){
+	for(j = 0;j < n;j++){
+	  cout << cplex.getValue(x[i][j]);
+	}
+	cout << endl;
+      }/**/
+
+      gnuplot_goldberg(I,p,&cplex,&x,&y);
+    }
+    else {
+      cout << "No solution found" << endl;
+    }
+    
+  }
+  catch (IloException& e) {
+    cerr << "Concert exception caught: " << e << endl;
+  }
+  catch (...) {
+    cerr << "Unknown exception caught" << endl;
+  }
+  env.end();
+  
+}
+
+void gnuplot_goldberg(SQM_instance *I,int p,IloCplex *cplex, IloBoolVarArray *x, BoolVarArrayMatrix *y) {
+  int i,j,k;
+  int n = I->N,m = I->M;
+  point *client = I->V;
+  point *potencial_site = I->W;
+  char outfilename[32],centersfilename[32],clientsfilename[32];
+
+  sprintf(centersfilename,"Tmp_centers_%d.dat",n);
+  ofstream centros(centersfilename);
+
+  for(j = 0;j < n;j++){
+    /*if (cplex->getValue((*x)[j]))*/
+    centros << potencial_site[j].x << " " 
+	    << potencial_site[j].y << " "
+	    << cplex->getValue((*x)[j]) << endl;
+  }
+  centros.close();
+
+  sprintf(clientsfilename,"Tmp_clients_%d.dat",n);
+  ofstream clients(clientsfilename);
+
+  for(i = 0;i < m;i++){
+    clients << client[i].x << " " << client[i].y << endl;
+  }
+  clients.close();
+
+  for (k = 0;k < p;k++) {
+    sprintf(outfilename,"Tmp_edges_%d_%d_%d.dat",m,n,k+1);
+    ofstream outfile(outfilename);
+
+    for(i = 0;i < m;i++){
+      for(j = 0;j < n;j++){
+	if (cplex->getValue((*y)[i][j][k]) > 0.5)
+	  outfile << client[i].x << " " 
+		  << client[i].y << " " 
+		  << potencial_site[j].x - client[i].x << " " 
+		  << potencial_site[j].y - client[i].y << " "
+		  << j+1 << endl;
+      }
+    }
+
+    outfile.close();
+  }
+
+  FILE *gnuPipe = popen("gnuplot","w");
+  fprintf(gnuPipe,"set term svg\n");
+  /*fprintf(gnuPipe,"set key outside\n");*/
+  fprintf(gnuPipe,"unset key\n");
+  fprintf(gnuPipe,"unset border\n");
+  fprintf(gnuPipe,"unset yzeroaxis\n");
+  fprintf(gnuPipe,"unset xtics\n");
+  fprintf(gnuPipe,"unset ytics\n");
+  fprintf(gnuPipe,"unset ztics\n");
+
+  /* plot full */
+  fprintf(gnuPipe,"set output 'Goldberg_%d_%d_%d.svg'\n",m,n,p);
+  fprintf(gnuPipe,"plot ");
+  fprintf(gnuPipe,"'%s' using 1:2 with points lc rgb \"black\" title 'Demand'",clientsfilename);
+  fprintf(gnuPipe,", '%s' using 1:2 with points lc rgb \"red\" title 'Facility'",centersfilename);
+  fprintf(gnuPipe,", '%s' using 1:($3 > 0.5 ? $2 : 1/0):(10) with circles lc rgb 'blue' title 'Opened'",centersfilename);
+  for (k = 0;k < p;k++) {
+    sprintf(outfilename,"Tmp_edges_%d_%d_%d.dat",m,n,k+1);
+    fprintf(gnuPipe,", '%s' using 1:2:3:4 with vectors nohead",outfilename);
+  }
+  fprintf(gnuPipe,"\n");
+
+  for (k = 0;k < p;k++) {
+    sprintf(outfilename,"Tmp_edges_%d_%d_%d.dat",m,n,k+1);
+    fprintf(gnuPipe,"set output 'Goldberg_%d_%d_%d_order_%02d.svg'\n",m,n,p,k+1);
+    fprintf(gnuPipe,"plot ");
+    fprintf(gnuPipe,"'%s' using 1:2 with points lc rgb \"black\"",clientsfilename);
+    fprintf(gnuPipe,", '%s' using 1:2 with points lc rgb \"red\"",centersfilename);
+    fprintf(gnuPipe,", '%s' using 1:2:3:4 with vectors nohead",outfilename);
+    fprintf(gnuPipe,"\n");
+  }
+
+  for (j = 0;j < n;j++) {
+    if (cplex->getValue((*x)[j]) > 0.5) {
+      for (k = 0;k < p;k++) {
+	sprintf(outfilename,"Tmp_edges_%d_%d_%d.dat",m,n,k+1);
+	fprintf(gnuPipe,"set output 'Goldberg_%d_%d_%d_center_%02d_order_%02d.svg'\n",m,n,p,j+1,k+1);
+	fprintf(gnuPipe,"plot ");
+	fprintf(gnuPipe,"'%s' using 1:2 with points lc rgb \"black\" title 'Demand'",clientsfilename);
+	fprintf(gnuPipe,", '%s' using 1:2 with points lc rgb \"red\" title 'Facility'",centersfilename);
+	fprintf(gnuPipe,", '%s' using 1:($5 == %d ? $2 : 1/0):3:4 with vectors nohead title 'Order %d'",outfilename,j+1,k+1);
+	fprintf(gnuPipe,"\n");
+      }
+    }
+  }
+
+  pclose(gnuPipe);
+  remove(clientsfilename);
+  remove(centersfilename);
+  for (k = 0;k < p;k++) {
+    sprintf(outfilename,"Tmp_edges_%d_%d_%d.dat",m,n,k+1);
+    remove(outfilename);
+  }
+}
---
+++ src/SQM_model.cpp
@@ -0,0 +1,595 @@
+/*
+ * Modelo de Aproxmiacion al problema de Berman
+ * 
+ */
+
+#include "SQM_model.h"
+
+void SQM_model
+(instance* I, // Set of points
+ int p, // facilities
+ float mu, // rate parameter
+ float f, // portion of demand
+ float speed) // speed
+{
+  IloEnv env;
+  try {
+    int i,j,l,k,r;
+    IloInt m,n = I->n;
+    IloNum f_i;
+    IloNum rho;
+    IloNum M = 10000.0;
+    IloNum beta = 1.5;
+    k = 3;
+    m = n;
+    point *puntos = I->points;
+    cout << "Comienza definicion del Modelo" << endl;
+    IloModel modelo(env);
+    
+    cout << "++ Variables ++" << endl;
+    IloIntVarArray x(env);
+    BoolVarArrayMatrix y(env,n);
+    BoolVarMatrix u(env,n);
+    BoolVarMatrix v(env,n);
+    
+    cout << "Nombre las variables para facil identificacion" << endl;
+    char VarName[16];
+    for(i = 0;i < n;i++){
+      sprintf(VarName,"x%d",i+1);
+      x.add(IloIntVar(env,VarName));
+    }
+
+    for (i = 0;i < n;i++) {
+      BoolVarMatrix y_i(env,n);
+      for (j = 0;j < n;j++) {
+	IloBoolVarArray y_i_j(env);
+	for (l = 0;l < k;l++) {
+	  sprintf(VarName,"y_%d_%d_%d",i+1,j+1,l+1);
+	  y_i_j.add(IloBoolVar(env,VarName));
+	}
+	y_i[j] = y_i_j;
+      }
+      y[i] = y_i;
+    }
+
+    for (i = 0;i < n;i++) {
+      IloBoolVarArray u_i(env);
+      for (j = 0;j < n;j++) {
+	sprintf(VarName,"u_%d_%d",i+1,j+1);
+	u_i.add(IloBoolVar(env,VarName));
+      }
+      u[i] = u_i;
+    }
+
+    for (i = 0;i < n;i++) {
+      IloBoolVarArray v_i(env);
+      for (j = 0;j < n;j++) {
+	sprintf(VarName,"v_%d_%d",i+1,j+1);
+	v_i.add(IloBoolVar(env,VarName));
+      }
+      v[i] = v_i;
+    }
+
+    cout << "++ Restricciones ++" << endl;
+    cout << "Instalaciones a abrir" << endl;
+    modelo.add(IloSum(x) == p);
+
+    cout << "Relacionar variables de localizacion y asignacion" << endl;
+    for(i = 0;i < n;i++){
+      for (j = 0;j < n;j++) {
+	for (l = 0;l < k;l++) 
+	  modelo.add(y[i][j][l] <= x[j]);
+      }
+    }
+
+    cout << "Solo una instalacion ocupa la posion k del cliente i" << endl;
+    for (i = 0;i < n;i++) {
+      for (l = 0;l < k;l++) {
+	IloExpr Cover(env);
+	for (j = 0;j < n;j++) Cover += y[i][j][l];
+	modelo.add(Cover == 1);
+	Cover.end();
+      }
+    }
+
+    cout << "Los ajustadores en j solo puede ocupar x_j posiciones del cliente i" << endl;
+    for (i = 0;i < n;i++) {
+      for (j = 0;j < n;j++) {
+	modelo.add(IloSum(y[i][j]) <= x[j]);
+      }
+    }
+
+    cout << "Restricion de orden de asignaicion" << endl;
+    NumMatrix O(env,n);
+    for (i = 0;i < n;i++)
+      O[i] = IloNumArray(env,n);
+    for (i = 0;i < n;i++) {
+      O[i][i] = 0;
+      for (j = i+1;j < n;j++) {
+	O[i][j] = dist(&(puntos[i]),&(puntos[j]));
+	O[j][i] = dist(&(puntos[i]),&(puntos[j]));
+      }
+    }
+      
+    /*  */
+    for (i = 0;i < n;i++) {
+      for (l = 1;l < k;l++) {
+	for (j = 0;j < m;j++) {
+	  IloExpr balance(env);
+	  for (r = 0;r < m;r++) {
+	    if (O[i][r] <= O[i][j])
+	      balance += y[i][r][l-1];
+	  }
+	  modelo.add(y[i][j][l] <= balance);
+	  balance.end();
+	}
+      }
+    } /* */
+
+    cout << "Restricciones disjuntas" << endl;
+    /* Restriccion para realacionar 
+       u_{ij} = 
+                1: Si la suma de instalaciones mas cercanas a i hasta incluir j es menor o igual a k
+		0: Si no
+    */
+    for (i = 0;i < n;i++) {
+      for (j = 0;j < m;j++) {
+	IloExpr Instalaciones(env);
+	for (r = 0;r < m;r++) {
+	  if (O[i][r] <= O[i][j])
+	    Instalaciones += x[r];
+	}
+	modelo.add(Instalaciones <= p - (p - k) * u[i][j]);
+	modelo.add(Instalaciones + M * u[i][j] >= k + 1);
+	Instalaciones.end();
+      }
+    }
+
+    /* Si u_{ij} = 1, todas las instalaciones de j deben de ser asignadas a i */
+    for (i = 0;i < n;i++) {
+      for (j = 0;j < m;j++) {
+	modelo.add(IloSum(y[i][j]) + M * (1 - u[i][j]) >= x[j]);
+      }
+    }
+
+    /* Restriccion para relacionar
+       v_{ij} =
+                1: Si la suma de instalaciones mas cercanas a i hasta antes de j es menor a k
+		0: Si no
+     */
+    for (i = 0;i < n;i++) {
+      for (j = 0;j < m;j++) {
+	if ( i != j) {
+	  IloExpr Instalaciones(env);
+	  for (r = 0;r < m;r++) {
+	    if (O[i][r] < O[i][j])
+	      Instalaciones += x[r];
+	  }
+	  modelo.add(Instalaciones <= p - (p - (k - 1)) * v[i][j]);
+	  modelo.add(Instalaciones + M * v[i][j] >= k);
+
+	  /* Si v_{ij} = 1 y u_{ij} = 0 alguntas de las instalaciones de j seran asignadas a i para completar las k */
+	  modelo.add(IloSum(y[i][j]) + M * (1 - v[i][j] + u[i][j]) >= k - Instalaciones);
+	  modelo.add(IloSum(y[i][j]) - M * (1 - v[i][j] + u[i][j]) <= k - Instalaciones);
+
+	  Instalaciones.end();
+	}
+      }
+    }
+
+    /* En caso de ser u_{ij} = 0 y v_{ij] = 0, no se asinga ninguna instalacion de j a i*/
+    for (i = 0;i < n;i++) {
+      for (j = 0;j < m;j++) {
+	for (l = 1;l < k;l++) {
+	  modelo.add(y[i][j][l] <= u[i][j] + v[i][j]);
+	}
+      }
+    }
+
+    /* Comienza definicion de funcion objetivo */
+    // Cargas de trabajo
+    IloNum coef;
+    // rho from Daskin
+    rho = 0.0;
+    for (i = 0;i < n;i++)
+      rho += f * puntos[i].demand;
+    rho /= (mu * p);
+    LogFile << "rho = " << rho << endl;
+    // rho from ReVelle & Hogan
+    // pendiente
+    
+    IloExpr workload(env);
+    IloNum time_per_km = beta / speed;
+    IloNum wt = 0.0;
+    for (l = 0;l < k;l++) {
+      coef = (1 - rho) * pow(rho,l);
+      for (i = 0;i < n;i++) {
+	f_i = f * puntos[i].demand;
+	for (j = 0;j < m;j++) {
+	  workload += f_i * coef * (O[i][j]*time_per_km + wt)* y[i][j][l];
+	}
+      }
+    }
+    
+    cout << "Funcion Objetivo" << endl;
+    modelo.add(IloMaximize(env,workload));
+    workload.end();
+
+    cout << "Solve model" << endl;
+    IloCplex cplex(modelo);
+    stringstream ModelName;
+    ModelName << "SQM-model_" << n << "_" << m << "_" << p << ".lp";
+    cplex.exportModel(ModelName.str().c_str());
+    cplex.setParam(IloCplex::TiLim,TIME_MAX);
+    if(cplex.solve()){
+      LogFile << "Solution status: " << cplex.getStatus() << endl;
+      LogFile << "Maximum profit = " << cplex.getObjValue() << endl;
+      for(j = 0;j < n;j++){
+	if(cplex.getValue(x[j]) > 0) {
+	  LogFile << j+1 << ": "
+		  << cplex.getValue(x[j]) << endl;
+	} 
+	//LogFile << cplex.getValue(y[j]);
+      }
+      LogFile << endl;
+      /*for(i = 0;i < n;i++){
+	LogFile << "-";
+      }
+      LogFile << endl;
+      for(i = 0;i < n;i++){
+	for(j = 0;j < n;j++){
+	  LogFile << cplex.getValue(x[i][j]);
+	}
+	LogFile << endl;
+      }/**/
+    }
+    else {
+      LogFile << "No solution found" << endl;
+    }
+    
+  }
+  catch (IloException& e) {
+    cerr << "Concert exception caught: " << e << endl;
+  }
+  catch (...) {
+    cerr << "Unknown exception caught" << endl;
+  }
+  env.end();
+  
+}
+
+int* SQM_model
+(SQM_instance* I, // Set of points
+ int p, // facilities
+ int k, // Number of facilities that care
+ float mu, // rate parameter
+ float f, // portion of demand
+ float speed) // speed
+{
+  int *Sol;
+  IloEnv env;
+  try {
+    clock_t clocks,start = clock();
+    int i,j,l,r;
+    IloInt m,n;
+    IloNum f_i;
+    IloNum rho;
+    IloNum M = 10000.0;
+    IloNum beta = 1.5;
+    m = I->M;
+    n = I->N;
+    point *clients = I->V;
+    cout << "Comienza definicion del Modelo" << endl;
+    IloModel modelo(env);
+    
+    cout << "++ Variables ++" << endl;
+    IloNumVar S(env,0,IloInfinity,IloNumVar::Float,"S");
+    IloIntVarArray x(env);
+    BoolVarArrayMatrix y(env,m);
+    BoolVarMatrix u(env,m);
+    BoolVarMatrix v(env,m);
+    
+    cout << "Nombre las variables para facil identificacion" << endl;
+    char VarName[16];
+    for(i = 0;i < n;i++){
+      sprintf(VarName,"x%d",i+1);
+      x.add(IloIntVar(env,VarName));
+    }
+
+    for (i = 0;i < m;i++) {
+      BoolVarMatrix y_i(env,n);
+      for (j = 0;j < n;j++) {
+	IloBoolVarArray y_i_j(env);
+	for (l = 0;l < k;l++) {
+	  sprintf(VarName,"y_%d_%d_%d",i+1,j+1,l+1);
+	  y_i_j.add(IloBoolVar(env,VarName));
+	}
+	y_i[j] = y_i_j;
+      }
+      y[i] = y_i;
+    }
+
+    for (i = 0;i < m;i++) {
+      IloBoolVarArray u_i(env);
+      for (j = 0;j < n;j++) {
+	sprintf(VarName,"u_%d_%d",i+1,j+1);
+	u_i.add(IloBoolVar(env,VarName));
+      }
+      u[i] = u_i;
+    }
+
+    for (i = 0;i < m;i++) {
+      IloBoolVarArray v_i(env);
+      for (j = 0;j < n;j++) {
+	sprintf(VarName,"v_%d_%d",i+1,j+1);
+	v_i.add(IloBoolVar(env,VarName));
+      }
+      v[i] = v_i;
+    }
+
+    cout << "++ Restricciones ++" << endl;
+    cout << "Instalaciones a abrir" << endl;
+    modelo.add(IloSum(x) == p);
+
+    cout << "Relacionar variables de localizacion y asignacion" << endl;
+    for(i = 0;i < m;i++){
+      for (j = 0;j < n;j++) {
+	for (l = 0;l < k;l++) 
+	  modelo.add(y[i][j][l] <= x[j]);
+      }
+    }
+
+    cout << "Solo una instalacion ocupa la posion k del cliente i" << endl;
+    for (i = 0;i < m;i++) {
+      for (l = 0;l < k;l++) {
+	IloExpr Cover(env);
+	for (j = 0;j < n;j++) Cover += y[i][j][l];
+	modelo.add(Cover == 1);
+	Cover.end();
+      }
+    }
+
+    cout << "Los ajustadores en j solo puede ocupar x_j posiciones del cliente i" << endl;
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	modelo.add(IloSum(y[i][j]) <= x[j]);
+      }
+    }
+
+    cout << "Restricion de orden de asignaicion" << endl;
+    NumMatrix O(env,m);
+    for (i = 0;i < m;i++)
+      O[i] = IloNumArray(env,n);
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	O[i][j] = dist(&(clients[i]),&((I->W)[j]));
+      }
+    }
+      
+    /*  */
+    for (i = 0;i < m;i++) {
+      for (l = 1;l < k;l++) {
+	for (j = 0;j < n;j++) {
+	  IloExpr balance(env);
+	  for (r = 0;r < n;r++) {
+	    if (O[i][r] <= O[i][j])
+	      balance += y[i][r][l-1];
+	  }
+	  modelo.add(y[i][j][l] <= balance);
+	  balance.end();
+	}
+      }
+    } /* */
+
+    cout << "Restricciones disjuntas" << endl;
+    /* Restriccion para realacionar 
+       u_{ij} = 
+                1: Si la suma de instalaciones mas cercanas a i hasta incluir j es menor o igual a k
+		0: Si no
+    */
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	IloExpr Instalaciones(env);
+	for (r = 0;r < n;r++) {
+	  if (O[i][r] <= O[i][j])
+	    Instalaciones += x[r];
+	}
+	modelo.add(Instalaciones <= p - (p - k) * u[i][j]);
+	modelo.add(Instalaciones + M * u[i][j] >= k + 1);
+	Instalaciones.end();
+      }
+    }
+
+    /* Si u_{ij} = 1, todas las instalaciones de j deben de ser asignadas a i */
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	modelo.add(IloSum(y[i][j]) + M * (1 - u[i][j]) >= x[j]);
+      }
+    }
+
+    /* Restriccion para relacionar
+       v_{ij} =
+                1: Si la suma de instalaciones mas cercanas a i hasta antes de j es menor a k
+		0: Si no
+     */
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	if ( i != j) {
+	  IloExpr Instalaciones(env);
+	  for (r = 0;r < n;r++) {
+	    if (O[i][r] < O[i][j])
+	      Instalaciones += x[r];
+	  }
+	  modelo.add(Instalaciones <= p - (p - (k - 1)) * v[i][j]);
+	  modelo.add(Instalaciones + M * v[i][j] >= k);
+
+	  /* Si v_{ij} = 1 y u_{ij} = 0 alguntas de las instalaciones de j seran asignadas a i para completar las k */
+	  modelo.add(IloSum(y[i][j]) + M * (1 - v[i][j] + u[i][j]) >= k - Instalaciones);
+	  modelo.add(IloSum(y[i][j]) - M * (1 - v[i][j] + u[i][j]) <= k - Instalaciones);
+
+	  Instalaciones.end();
+	}
+      }
+    }
+
+    /* En caso de ser u_{ij} = 0 y v_{ij] = 0, no se asinga ninguna instalacion de j a i*/
+    for (i = 0;i < m;i++) {
+      for (j = 0;j < n;j++) {
+	for (l = 1;l < k;l++) {
+	  modelo.add(y[i][j][l] <= u[i][j] + v[i][j]);
+	}
+      }
+    }
+
+    /* Comienza definicion de funcion objetivo */
+    // Cargas de trabajo
+    IloNum coef;
+    // rho from Daskin
+    rho = 0.0;
+    for (i = 0;i < m;i++)
+      rho += f * clients[i].demand;
+    rho /= (mu * p);
+    LogFile << "rho = " << rho << endl;
+    // rho from ReVelle & Hogan
+    // pendiente
+    
+    IloNum time_per_km = beta / speed;
+    IloNum wt = 0.0;
+    for (j = 0;j < n;j++) {
+      IloExpr workload(env);
+      for (l = 0;l < k;l++) {
+	coef = (1 - rho) * pow(rho,l);
+	for (i = 0;i < m;i++) {
+	  f_i = f * clients[i].demand;
+	  workload += f_i * coef * (O[i][j]*time_per_km + wt)* y[i][j][l];
+	}
+      }
+      modelo.add(S >= workload);
+    }
+    
+    cout << "++ Funcion Objetivo ++" << endl;
+    modelo.add(IloMinimize(env,S));
+    clocks = clock() - start;
+    results << "," << clocks / CLOCKS_PER_SEC;
+
+    IloCplex cplex(modelo);
+    stringstream ModelName;
+    ModelName << "SQM-model_" << m << "_" << n << "_" << p << "_" << k << ".lp";
+    cplex.exportModel(ModelName.str().c_str());
+    results << "," << ModelName.str();
+
+    LogFile << "Solve SQM model" << endl;
+    LogFile << endl << "** Cplex Start **" << endl;
+    cplex.setOut(LogFile);
+    cplex.setParam(IloCplex::TiLim,TIME_MAX);
+
+    double CplexTime = cplex.getCplexTime();
+    if(cplex.solve()){
+
+      double gap = (cplex.getObjValue() - cplex.getBestObjValue()) / cplex.getBestObjValue();
+      results << "," << cplex.getCplexTime() - CplexTime
+	      << "," << cplex.getStatus()
+	      << "," << cplex.getObjValue()
+	      << "," << gap
+	      << endl;
+
+      Sol = new int[n];
+      LogFile << "** Cplex Ends **" << endl << endl;
+      LogFile << "Solution status: " << cplex.getStatus() << endl;
+      LogFile << "Maximum profit = " << cplex.getObjValue() << endl;
+      for(j = 0;j < n;j++){
+	Sol[j] = 0;
+	if(cplex.getValue(x[j]) > 0.5) {
+	  Sol[j] = cplex.getValue(x[j]);
+	  LogFile << j+1 << ": "
+	       << Sol[j] << endl;
+	    } 
+	//LogFile << cplex.getValue(y[j]);
+      }
+      LogFile << endl;
+      /*for(i = 0;i < n;i++){
+	LogFile << "-";
+      }
+      LogFile << endl;
+      for(i = 0;i < n;i++){
+	for(j = 0;j < n;j++){
+	  LogFile << cplex.getValue(x[i][j]);
+	}
+	LogFile << endl;
+      }/**/
+    }
+    else {
+      LogFile << "No solution found" << endl;
+      LogFile << "** Cplex Ends **" << endl << endl;
+      Sol = NULL;
+    }
+    
+  }
+  catch (IloException& e) {
+    cerr << "Concert exception caught: " << e << endl;
+    Sol = NULL;
+  }
+  catch (...) {
+    cerr << "Unknown exception caught" << endl;
+    Sol = NULL;
+  }
+  env.end();
+  return Sol;
+}
+
+void gnuplot_goldberg(instance *I,int p,IloCplex *cplex, IloBoolVarArray *x, BoolVarArrayMatrix *y) {
+  int i,j,k;
+  int n = I->n;
+  point *puntos = I->points;
+
+  char outfilename[32],centersfilename[32];
+
+  sprintf(centersfilename,"../Instancias/Centros_%d.dat",n);
+  ofstream centros(centersfilename);
+
+  for(i = 0;i < n;i++){
+    if (cplex->getValue((*x)[i]))
+      centros << puntos[i].x << " " << puntos[i].y << endl;
+  }
+  centros.close();
+
+  for (k = 0;k < p;k++) {
+    sprintf(outfilename,"../Instancias/Ejes_%d_%d.dat",n,k+1);
+    ofstream outfile(outfilename);
+
+    for(i = 0;i < n;i++){
+      for(j = 0;j < n;j++){
+	if (i != j && cplex->getValue((*y)[i][j][k]))
+	  outfile << puntos[i].x << " " 
+		  << puntos[i].y << " " 
+		  << puntos[j].x - puntos[i].x << " " 
+		  << puntos[j].y - puntos[i].y << endl;
+      }
+    }
+
+    outfile.close();
+  }
+
+  FILE *gnuPipe = popen("gnuplot","w");
+  fprintf(gnuPipe,"set term svg\n");
+  fprintf(gnuPipe,"set output '../gnuplot/Goldberg_%d_%d.svg'\n",n,p);
+  fprintf(gnuPipe,"unset key\n");
+  fprintf(gnuPipe,"unset border\n");
+  fprintf(gnuPipe,"unset yzeroaxis\n");
+  fprintf(gnuPipe,"unset xtics\n");
+  fprintf(gnuPipe,"unset ytics\n");
+  fprintf(gnuPipe,"unset ztics\n");
+  //fprintf(gnuPipe,"set title \"Servicio de %.0f\n",cplex->getObjValue());
+  //fprintf(gnuPipe,"set style arrow 1 nohead lw 2\n");
+  //fprintf(gnuPipe,"set arrow arrowstyle 1\n");
+  fprintf(gnuPipe,"plot ");
+  fprintf(gnuPipe,"'../Instancias/Q_MCLP_%d.txt' every ::1 using 1:2 with points lc rgb \"black\"",n);
+  fprintf(gnuPipe,", '../Instancias/Centros_%d.dat' using 1:2 with points lc rgb \"red\"",n);
+  for (k = 0;k < p;k++) {
+    fprintf(gnuPipe,", '../Instancias/Ejes_%d_%d.dat' using 1:2:3:4 with vectors nohead" /* linecolor rgb \"dark-blue\"" */ ,n,k+1);
+  }
+
+  fprintf(gnuPipe,"\n");
+  pclose(gnuPipe);
+
+}
