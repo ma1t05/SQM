3982c51797d4831a4184c6edbd0f7e2b4affef9d
ma1t05 <maltosla@gmail.com>
2016-03-01 20:44:05 -0600

    Remove overload definition
    The definition for the operator '++' was defined most than once

--- include/PathRelinking.h
+++ include/PathRelinking.h
@@ -18,7 +18,6 @@ enum matching_type {perfect_matching,workload_matching,random_matching,
 		    invalid_matching};
 matching_type& operator++(matching_type&);
 extern int* (*matching_function)(SQM_solution&,SQM_solution&);
-matching_type& operator++(matching_type& target);
 int* PR_perfect_matching(SQM_solution&,SQM_solution&);
 int* PR_random_matching(SQM_solution&,SQM_solution&);
 int* PR_workload_matching(SQM_solution&,SQM_solution&);
