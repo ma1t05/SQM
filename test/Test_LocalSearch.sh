d6bcc286a4552642b0af6c467ebccabb991a8148
ma1t05 <maltosla@gmail.com>
2016-03-02 11:27:44 -0600

    Increase the number of test cases

--- test/Test_LocalSearch.sh
+++ test/Test_LocalSearch.sh
@@ -3,18 +3,21 @@
 cd ./../bin
 SQM=./SQM
 
-prefix=Test_01
-M=50
-N=50
-p=10
 k=3
 l=6
 m=3
 s=500.0
 
 
-for prefix in Test_01 Test_02; do
-    options="-f$prefix -M$M -N$N -p$p -k$k -l$l -m$m -s$s"
-    $SQM $options --brief Local_Search
+# Model
+for M in 50 100 150; do
+    for N in 30 50 75; do
+	for p in 7 10 15 20; do
+	    for i in {01..05}; do
+		prefix=Test_${M}_${N}_${p}_${i}
+		options="-f$prefix -M$M -N$N -p$p -k$k -l$l -m$m -s$s"
+		$SQM $options --superbrief Local_Search
+	    done
+	done
+    done
 done
-
